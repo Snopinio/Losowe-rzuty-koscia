5ec28d159f0d052644942da7b6cebcff9e1814ec
Snopinio <jansnopkowski64@gmail.com>
2023-03-24 10:27:50 +0100

    nowawersja

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -5,25 +5,27 @@ Console.WriteLine("2. K4");
 Console.WriteLine("3. K6");
 Console.WriteLine("4. K100");
 
-string choice;
-choice = Console.ReadLine();
+char choice;
+choice = Console.ReadKey(true).KeyChar;
+//pusta linia
+Console.WriteLine();
 Random rng = new Random();
 
 switch(choice)
 {
-    case "1":
+    case '1':
         // jesli podano 1
         Console.WriteLine("Wynik rzutu K3: " + rng.Next(1,3));
         break;
-    case "2":
+    case '2':
         // jesli podano 2
         Console.WriteLine("Wynik rzutu K3: " + rng.Next(1, 4));
         break;
-    case "3":
+    case '3':
         //jesli podano 3
         Console.WriteLine("Wynik rzutu K3: " + rng.Next(1, 6));
         break;
-    case "4":
+    case '4':
         // jesli podano 4
         Console.WriteLine("Wynik rzutu K3: " + rng.Next(1, 100));
         break;
