6072e0b57a22f339681764ccc61c655b7ffd6dd8
Snopinio <jansnopkowski64@gmail.com>
2023-03-24 10:37:07 +0100

    case z zamykaniem programu

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,36 +1,47 @@
 ﻿// Prosta aplikacja wykonujace pseudolosowe rzuty wybrana koscia
-Console.WriteLine("Jaką kością chcesz rzucić: ");
-Console.WriteLine("1. K3");
-Console.WriteLine("2. K4");
-Console.WriteLine("3. K6");
-Console.WriteLine("4. K100");
+
 
 char choice;
-choice = Console.ReadKey(true).KeyChar;
-//pusta linia
-Console.WriteLine();
+
 Random rng = new Random();
 
-switch(choice)
+do
 {
-    case '1':
-        // jesli podano 1
-        Console.WriteLine("Wynik rzutu K3: " + rng.Next(1,3));
-        break;
-    case '2':
-        // jesli podano 2
-        Console.WriteLine("Wynik rzutu K3: " + rng.Next(1, 4));
-        break;
-    case '3':
-        //jesli podano 3
-        Console.WriteLine("Wynik rzutu K3: " + rng.Next(1, 6));
-        break;
-    case '4':
-        // jesli podano 4
-        Console.WriteLine("Wynik rzutu K3: " + rng.Next(1, 100));
-        break;
-    default:
-        //wykona sie jesli zaden wczesniejszy nie pasuje
-        Console.WriteLine("Podano nieprawidłową opcję!");
-        break;
+    Console.WriteLine("Jaką kością chcesz rzucić: ");
+    Console.WriteLine("1. K3");
+    Console.WriteLine("2. K4");
+    Console.WriteLine("3. K6");
+    Console.WriteLine("4. K100");
+    Console.WriteLine("0. Wyjscie");
+    choice = Console.ReadKey(true).KeyChar;
+    switch (choice)
+    {
+        case '1':
+            // jesli podano 1
+            Console.WriteLine("Wynik rzutu K3: " + rng.Next(1, 3));
+            break;
+        case '2':
+            // jesli podano 2
+            Console.WriteLine("Wynik rzutu K3: " + rng.Next(1, 4));
+            break;
+        case '3':
+            //jesli podano 3
+            Console.WriteLine("Wynik rzutu K3: " + rng.Next(1, 6));
+            break;
+        case '4':
+            // jesli podano 4
+            Console.WriteLine("Wynik rzutu K3: " + rng.Next(1, 100));
+            break;
+        case '0':
+            //jesli podano 0
+            Console.WriteLine("Zamykanie programu");
+            break;
+        default:
+            //wykona sie jesli zaden wczesniejszy nie pasuje
+            Console.WriteLine("Podano nieprawidłową opcję!");
+            break;
+    }
 }
+while (choice != '0');
+
+
